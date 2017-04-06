057b8758fff4f368db180fec1c66c00715951004
mokamoto12 <okamoto_masahiro@aratana.jp>
2017-04-06 18:34:12 +0900

    Add solution interface.

---
+++ php/solutions/Interfaces/SolutionInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Solutions\Interfaces;
+
+interface SolutionInterface
+{
+    /**
+     * Resolve problem
+     *
+     * @return mixed
+     */
+    public static function resolve();
+}
